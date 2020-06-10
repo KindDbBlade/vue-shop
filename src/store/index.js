f69ed5a6fac789343fad733eb713a8f458a82ed1
周绪明 <z19961106xm@163.com>
2020-06-10 09:24:31 +0800

    aside

--- src/store/index.js
+++ src/store/index.js
@@ -6,31 +6,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   //username 为用法示例
   state: {
-    username:"zxm"
   },
   getters:{
-    grtusername: state => {
-      return state.username;
-    }
   },
   mutations: {
-    //同步
-    setusername(state){
-      state.username = state.username
-    },
-    removeUsername(state,othername){
-      state.username = othername
-    }
   },
   actions: {
-    //异步  触发改变
-    setusernameFun(context){
-      context.commit("setusername")
-    },
-    removeUsernameFun(context,othername){
-      context.commit("removeUsername",othername);
-    }
-
   },
   modules: {
   }
